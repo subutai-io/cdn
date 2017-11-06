7e8baf1431e75072bb5320493e3a7f02345cf700
soffokl <soffokulus@gmail.com>
2017-11-06 11:59:50 +0600

    Added hostname replacement for revproxy. #127
    Signed-off-by: soffokl <soffokulus@gmail.com>

--- main.go
+++ main.go
@@ -25,10 +25,24 @@ func main() {
 	// go torrent.SeedLocal()
 
 	if len(config.CDN.Node) > 0 {
-		proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-			Scheme: "https",
-			Host:   config.CDN.Node,
-		})
+		target := url.URL{Scheme: "https", Host: config.CDN.Node}
+		proxy := httputil.NewSingleHostReverseProxy(&target)
+		targetQuery := target.RawQuery
+		proxy.Director = func(req *http.Request) {
+			req.URL.Scheme = target.Scheme
+			req.URL.Host = target.Host
+			req.Host = config.CDN.Node
+			req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+			if targetQuery == "" || req.URL.RawQuery == "" {
+				req.URL.RawQuery = targetQuery + req.URL.RawQuery
+			} else {
+				req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+			}
+			if _, ok := req.Header["User-Agent"]; !ok {
+				// explicitly disable User-Agent so it's not set to default value
+				req.Header.Set("User-Agent", "")
+			}
+		}
 		log.Check(log.ErrorLevel, "Starting to listen :"+config.Network.Port, http.ListenAndServe(":"+config.Network.Port, proxy))
 		return
 	}
@@ -81,4 +95,16 @@ func about(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
 	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
