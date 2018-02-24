1c9bc37cf645a6b017ed78f4830e18ae73b4e73f
emli <esulaymanov@optimal-dynamics.com>
2018-02-24 12:10:26 +0600

    #254
    When apt file will deleted Packages file should updated
    
    Add update Packages and sign Release file after deleting artifact.

--- apt/apt.go
+++ apt/apt.go
@@ -2,7 +2,6 @@ package apt
 
 import (
 	"archive/tar"
-	"bufio"
 	"bytes"
 	"compress/gzip"
 	"io"
@@ -78,28 +77,6 @@ func getSize(file string) (size string) {
 	return size
 }
 
-func writePackage(meta map[string]string) {
-	var f *os.File
-	if _, err := os.Stat(config.Storage.Path + "Packages"); os.IsNotExist(err) {
-		f, err = os.Create(config.Storage.Path + "Packages")
-		log.Check(log.WarnLevel, "Creating packages file", err)
-		defer f.Close()
-	} else if err == nil {
-		f, err = os.OpenFile(config.Storage.Path+"Packages", os.O_APPEND|os.O_WRONLY, 0600)
-		log.Check(log.WarnLevel, "Opening packages file", err)
-		defer f.Close()
-	} else {
-		log.Warn(err.Error())
-	}
-
-	for k, v := range meta {
-		_, err := f.WriteString(string(k) + ": " + string(v) + "\n")
-		log.Check(log.WarnLevel, "Appending package data", err)
-	}
-	_, err := f.Write([]byte("\n"))
-	log.Check(log.WarnLevel, "Appending endline", err)
-}
-
 func Upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		_, header, _ := r.FormFile("file")
@@ -147,65 +124,10 @@ func Download(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func readPackages() []string {
-	file, err := os.Open(config.Storage.Path + "Packages")
-	log.Check(log.WarnLevel, "Opening packages file", err)
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	log.Check(log.WarnLevel, "Scanning packages list", scanner.Err())
-	return lines
-}
-
-func deleteInfo(hash string) {
-	list := readPackages()
-	if len(list) == 0 {
-		log.Warn("Empty packages list")
-		return
-	}
-
-	var newlist, block string
-	changed, skip := false, false
-	for _, line := range list {
-		if len(line) != 0 && skip {
-			continue
-		} else if len(line) == 0 {
-			skip = false
-			if len(block) != 0 {
-				newlist = newlist + block + "\n"
-				block = ""
-			}
-		} else if len(line) != 0 && !skip {
-			if strings.HasSuffix(line, hash) {
-				block = ""
-				skip = true
-				changed = true
-			} else {
-				block = block + line + "\n"
-			}
-		}
-	}
-	if changed {
-		log.Info("Updating packages list")
-		file, err := os.Create(config.Storage.Path + "Packages.new")
-		log.Check(log.WarnLevel, "Opening packages file", err)
-		defer file.Close()
-
-		_, err = file.WriteString(newlist)
-		log.Check(log.WarnLevel, "Writing new list", err)
-		log.Check(log.WarnLevel, "Replacing old list",
-			os.Rename(config.Storage.Path+"Packages.new", config.Storage.Path+"Packages"))
-	}
-}
-
 func Delete(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "DELETE" {
 		if hash := upload.Delete(w, r); len(hash) != 0 {
-			deleteInfo(hash)
+			generateReleaseFile()
 			w.Write([]byte("Removed"))
 			return
 		}
