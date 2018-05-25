3d8eebd9426e072f6458d7a7a4b8fbc63cc667aa
azretkenzhaliev <azret.kenzhaliev@gmail.com>
2018-05-25 16:20:01 +0600

    added skeletons for info and list callbacks and request parser

--- server/callbacks.go
+++ server/callbacks.go
@@ -1,1 +1,36 @@
 package server
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func Info(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Incorrect method for info request"))
+		return fmt.Errorf("incorrect method for info request: use GET method")
+	}
+	var req SearchRequest
+	err := req.ParseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return err
+	}
+	return nil
+}
+
+func List(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Incorrect method for info request"))
+		return fmt.Errorf("incorrect method for info request: use GET method")
+	}
+	var req SearchRequest
+	err := req.ParseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return err
+	}
+	return nil
+}
--- server/retrieve.go
+++ server/retrieve.go
@@ -1,1 +1,61 @@
 package server
+
+import (
+	"net/http"
+	"strings"
+)
+
+type SearchRequest struct {
+	fileID  string
+	name    string
+	owner   string
+	token   string
+	tags    string
+	version string
+	repo    string
+}
+
+// ParseRequest takes HTTP request and converts it into Request struct
+func (r *SearchRequest) ParseRequest(req *http.Request) (err error) {
+	r.fileID = req.URL.Query().Get("id")
+	r.name = req.URL.Query().Get("name")
+	r.owner = req.URL.Query().Get("owner")
+	r.token = req.URL.Query().Get("token")
+	r.tags = req.URL.Query().Get("tags")
+	r.version = req.URL.Query().Get("version")
+	r.repo = strings.Split(req.RequestURI, "/")[3] // Splitting /kurjun/rest/repo/func into ["", "kurjun", "rest", "repo" (index: 3), "func"]
+	return
+}
+
+func (r *SearchRequest) BuildQuery() (query map[string]string) {
+	if r.fileID != "" {
+		query["fileID"] = r.fileID
+	}
+	if r.name != "" {
+		query["name"] = r.name
+	}
+	if r.owner != "" {
+		query["owner"] = r.owner
+	}
+	if r.token != "" {
+		query["token"] = r.token
+	}
+	if r.tags != "" {
+		query["tags"] = r.tags
+	}
+	if r.version != "" {
+		query["version"] = r.version
+	}
+	if r.repo != "" {
+		query["repo"] = r.repo
+	}
+	return
+}
+
+type SearchResult struct {
+
+}
+
+func Retrieve() {
+
+}
