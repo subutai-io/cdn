36de794ca1ab09d21cc64faa70a8ebfcce81397a
azretkenzhaliev <azret.kenzhaliev@gmail.com>
2018-05-29 10:36:00 +0600

    added ParseRequest for upload

--- server/app/upload.go
+++ server/app/upload.go
@@ -1,13 +1,34 @@
 package app
 
+import (
+	"io"
+	"net/http"
+	"fmt"
+)
+
 type UploadRequest struct {
-	file
+	file    io.Reader
 	token   string
 	private string
 }
 
-func (r *UploadRequest) ParseRequest()
+func (request *UploadRequest) ParseRequest(r *http.Request) error {
+	r.ParseMultipartForm(32 << 20)
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		return err
+	}
+	request.file = io.Reader(file) // multipart.sectionReadCloser
+	request.token = r.Header.Get("token")
+	if len(request.token) == 0 {
+		return fmt.Errorf("token for upload wasn't provided")
+	}
+	if len(r.MultipartForm.Value["private"]) > 0 {
+		request.private = r.MultipartForm.Value["private"][0]
+	}
+	return nil
+}
 
-func Upload(request ActionRequest) {
+func Upload(request UploadRequest) {
 
 }
