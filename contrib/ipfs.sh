12ecfe6d23115a3d7079f6d71e41abd0b2a43494
Eldar Tursunbaev <etursunbaev@optimal-dynamics.com>
2018-12-18 13:15:26 +0600

    fixed script

--- contrib/ipfs.sh
+++ contrib/ipfs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-objects=(QmepSBbWspyPkiQFMqkrqKeZpFr913VLrASnVqCGU1gW4c Qme9M21vV6q6GcXTiwWdW2dTFHMxRtsyco5EzJQw3QcNpC QmRQe2U3mzCUJLfg233mJRYDsYyYdJaA9b1wS5vbSXupur QmeKcFb8Bsg1mfn7aWuykduX75JBfBvZJifxbwsr9eGg7h QmetLuhCj5FrF5Xcu2d5FGyxsu4GWtsjGjFSi3FXtR73mY QmeZ916vgARQkTWup4whQnsoMGc74MeB89mghUzcPLhwGM QmS3apSdNsCkmeWmvDfMNoyQErKNZCUjDBUWFoersC75Qg QmZqg836oPxRKaK29q4ZHENWi8TzttYEeWX8qMAvxCH5jk QmYwGCDZVHMrFdUn67cFU5VX8knLXpAPupKTPQzbPMVgxq Qmd3y6iWnra9VZ6RuVBiMsbhahE9fb2sHMKWy89j7vNu6a Qmdxvze7CswmLAnNu2Wh9ptDhtykL8SP5xqQBmzZn76iGB QmQ8TYJCPH3EwL4aro9Hfwccie2RBRkstz34Xf8q2kaqKf QmZpqKxkn7dVB4sdSZKsEEvairPoXFSnBypp3bMzheybSD QmTHXFEYT6TUv2WYxx8VqSBAp6EmUCF8PxB7a3KjHpAKer QmP1y96Ggat3CyXRze8wrrnEwqtbW8THike2WVK1zdXhSy QmXj1QBTgG7f9cTTKkojh6UswR6fRsAbGPE4dEh9uhZDRv QmVZGtkVRe67nU1fy463X4tyWWh1Ph9nXSTPCHbLcr5mdP QmXzW9AMN3cYG7PqGfgW7UUst47QWieN855oYbXGPZJrCg QmVAnE9PXWJZoX7GP1vMq8fg2go5n5xcDC4tG6sdaX8zQb QmedMn7vhM1PUG7EEo99tD9WAqQMUEbyqHjaz33mzXQkwu)
+objects=( "QmepSBbWspyPkiQFMqkrqKeZpFr913VLrASnVqCGU1gW4c" "Qme9M21vV6q6GcXTiwWdW2dTFHMxRtsyco5EzJQw3QcNpC" "QmRQe2U3mzCUJLfg233mJRYDsYyYdJaA9b1wS5vbSXupur" "QmeKcFb8Bsg1mfn7aWuykduX75JBfBvZJifxbwsr9eGg7h" "QmetLuhCj5FrF5Xcu2d5FGyxsu4GWtsjGjFSi3FXtR73mY" "QmeZ916vgARQkTWup4whQnsoMGc74MeB89mghUzcPLhwGM" "QmS3apSdNsCkmeWmvDfMNoyQErKNZCUjDBUWFoersC75Qg" "QmZqg836oPxRKaK29q4ZHENWi8TzttYEeWX8qMAvxCH5jk" "QmYwGCDZVHMrFdUn67cFU5VX8knLXpAPupKTPQzbPMVgxq" "Qmd3y6iWnra9VZ6RuVBiMsbhahE9fb2sHMKWy89j7vNu6a" "Qmdxvze7CswmLAnNu2Wh9ptDhtykL8SP5xqQBmzZn76iGB" "QmQ8TYJCPH3EwL4aro9Hfwccie2RBRkstz34Xf8q2kaqKf" "QmZpqKxkn7dVB4sdSZKsEEvairPoXFSnBypp3bMzheybSD" "QmTHXFEYT6TUv2WYxx8VqSBAp6EmUCF8PxB7a3KjHpAKer" "QmP1y96Ggat3CyXRze8wrrnEwqtbW8THike2WVK1zdXhSy" "QmXj1QBTgG7f9cTTKkojh6UswR6fRsAbGPE4dEh9uhZDRv" "QmVZGtkVRe67nU1fy463X4tyWWh1Ph9nXSTPCHbLcr5mdP" "QmXzW9AMN3cYG7PqGfgW7UUst47QWieN855oYbXGPZJrCg" "QmVAnE9PXWJZoX7GP1vMq8fg2go5n5xcDC4tG6sdaX8zQb" "QmedMn7vhM1PUG7EEo99tD9WAqQMUEbyqHjaz33mzXQkwu" )
 
-names=(vagrant-subutai-stretch-virtualbox.box vagrant-subutai-stretch-libvirt.box vagrant-subutai-stretch-hyperv.box vagrant-subutai-stretch-parallels.box vagrant-subutai-stretch-vmware.box management generic-ansible debian-stretch XQuartz.pkg googlechrome.dmg Firefox-61.0.1.dmg vmware-fusion-osx.dmg 	VirtualBox.pkg vmware-workstation-windows.exe vagrant.msi 	VirtualBox.exe 	vmware-workstation-linux.bundle vagrant.deb virtualbox-xenial.deb virtualbox-stretch.deb)
+names=( "vagrant-subutai-stretch-virtualbox.box" "vagrant-subutai-stretch-libvirt.box" "vagrant-subutai-stretch-hyperv.box" "vagrant-subutai-stretch-parallels.box" "vagrant-subutai-stretch-vmware.box" "management" "generic-ansible" "debian-stretch" "XQuartz.pkg" "googlechrome.dmg" "Firefox-61.0.1.dmg" "vmware-fusion-osx.dmg" "VirtualBox.pkg" "vmware-workstation-windows.exe" "vagrant.msi" "VirtualBox.exe" "vmware-workstation-linux.bundle" "vagrant.deb" "virtualbox-xenial.deb" "virtualbox-stretch.deb" )
 
 n=0
 
 while [ $n -lt ${#objects[@]} ] ; do
-    DISPLAY_NAME[$n]=${names[$n]}
-    for object in $objects 
+    DISPLAY_NAME[$n]=${objects[$n]}" "${names[$n]}
+    for object in ${objects[$n]} 
         do 
             echo "Pinning ${DISPLAY_NAME[$n]}"
             ipfs pin add --progress /ipfs/${object}
                 if [ $? -ne 0 ]; then
                 echo "Sorry, the object ${object} is not pinned"
-                #exit 1
+                exit 0
                 fi
         done
     let n=n+1
